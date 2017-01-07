bfbfca41ace1e73becae1608c32d6f1f8424ddcf
caiohsouza <caiohenriqueads@gmail.com>
2017-01-07 00:29:49 -0300

    mais layout

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,7 @@
 class Classroom < ActiveRecord::Base
   belongs_to :unity
 
-  enum turn: { manha: 1, tarde: 2, noite: 3 }
+  enum turn: { Manha: 1, Tarde: 2, Noite: 3 }
 
   validates_presence_of :name, :turn, :unity_id
 end
